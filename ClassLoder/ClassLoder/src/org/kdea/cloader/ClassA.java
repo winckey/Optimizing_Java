2354c85fb21b4d114e07e06eb0ecf26a96c85434
DESKTOP-PJ3UT06\JangHyunJin <winckey0@naver.com>
2022-09-15 23:05:11 +0900

    Chater2 : 클래스로더 실습

---
+++ ClassLoder/ClassLoder/src/org/kdea/cloader/ClassA.java
@@ -0,0 +1,16 @@
+package org.kdea.cloader;
+
+public class ClassA
+{
+    static{	System.out.println("* ClassA 로드 완료 *"); }
+
+    public ClassA(){
+        System.out.println("ClassA 인스턴스 생성");
+    }
+
+    public void createB()
+    {
+        System.out.println("ClassA.createB() 실행됨");
+        ClassB b = new ClassB();
+    }
+}
---
+++ ClassLoder/ClassLoder/src/org/kdea/cloader/Main.java
@@ -0,0 +1,17 @@
+package org.kdea.cloader;
+
+
+
+public class Main
+{
+    static{	System.out.println("* Main 로드  완료 *"); }
+
+    public static void main(String[] args)
+    {
+        ClassA a = new ClassA();
+        a.createB();
+
+        ClassA a2 = new ClassA();
+        a2.createB();
+    }
+}
